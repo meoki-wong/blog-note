2b6588b2ee8fa5b68fe1eed5a8c0ad92edab76e0
meoki-wong <1084415961@qq.com>
2023-02-14 18:16:21 +0800

    feat: 添加操作区

--- src/views/note/components/NavContent.vue
+++ src/views/note/components/NavContent.vue
@@ -10,17 +10,29 @@
                 :key="index"
                 :style="index === navIndex? 'backgroundColor: #eee': ''"
                 @click="clickNavItem(item, index)"
-            ><span class="text-log" v-show="index === navIndex"></span>{{item}}</li>
+            ><span
+                    class="text-log"
+                    v-show="index === navIndex"
+                ></span>{{item}}</li>
+        </ul>
+        <ul class="option-contain">
+            <li
+                v-for="(item, index) in optionList"
+                :key="item.idx"
+                @click="touchOption(item.idx)"
+            >{{item.name}}</li>
         </ul>
     </div>
 </template>
 
 <script>
+import { optionList } from "./utils/common";
 export default {
     data() {
         return {
             navList: [],
-            navIndex: 0, // 选中的index是多少
+            navIndex: null, // 选中的index是多少
+            optionList, // option列表
         };
     },
     mounted() {
@@ -31,10 +43,13 @@ export default {
             let res = await this.axios.post("getMarkdownNavList");
             this.navList = res.data.data;
         },
-        async clickNavItem(item, index){
-          this.navIndex = index
-          let res = await this.axios.post('/getMarkdown', {filename: item})
-          this.$emit('innerNote', res.data.data)
+        async clickNavItem(item, index) {
+            this.navIndex = index;
+            let res = await this.axios.post("/getMarkdown", { filename: item });
+            this.$emit("innerNote", res.data.data);
+        },
+        touchOption(type){
+          console.log('---type', type);
         }
     },
 };
@@ -45,6 +60,7 @@ export default {
     width: 15%;
     border-right: 1px solid #eee;
     height: 100vh;
+    position: relative;
     .tabs-list {
         border-bottom: 2px solid #eee;
         display: flex;
@@ -57,29 +73,47 @@ export default {
             height: 50px;
         }
     }
-    .nav-list{
-      li{
-        cursor: pointer;
-        width: 100%;
-        height: 40px;
-        padding-left: 10px;
-        box-sizing: border-box;
-        text-align: left;
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        overflow:hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        -o-text-overflow:ellipsis;
-        .text-log{
-          width: 10px;
-          height: 100%;
-          background-color: #000;
-          display: inline-block;
-          margin: 0 20px 0 -10px;
+    .nav-list {
+        li {
+            cursor: pointer;
+            width: 100%;
+            height: 40px;
+            padding-left: 10px;
+            box-sizing: border-box;
+            text-align: left;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            -o-text-overflow: ellipsis;
+            .text-log {
+                width: 10px;
+                height: 100%;
+                background-color: #000;
+                display: inline-block;
+                margin: 0 20px 0 -10px;
+            }
         }
-      }
+    }
+    .option-contain {
+        position: absolute;
+        right: -50px;
+        bottom: 50px;
+        list-style: none;
+        li {
+            width: 50px;
+            height: 30px;
+            border-radius: 0 14px 14px 0;
+            border: 1px solid #eee;
+            border-left: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 10px;
+            cursor: pointer;
+        }
     }
 }
 </style>
---
+++ src/views/note/components/utils/common.js
@@ -0,0 +1,6 @@
+export const optionList = [
+	{ name: "测试", icon: "", idx: 0, attr: ""},
+	{ name: "测试", icon: "", idx: 1, attr: "" },
+	{ name: "测试", icon: "", idx: 2, attr: "" },
+	{ name: "测试", icon: "", idx: 3, attr: "" },
+];
